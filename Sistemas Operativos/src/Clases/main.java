f470c1950a448f61f65270a1d9f607ca523af488
AndreRoj <130945779+AndreRoj@users.noreply.github.com>
2025-11-04 20:15:25 -0400

    Pruebita

---
+++ Sistemas Operativos/src/Clases/main.java
@@ -0,0 +1,23 @@
+package Clases;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author pjroj
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
