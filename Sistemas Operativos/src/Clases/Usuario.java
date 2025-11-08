2d871c853c68697a708efbe3a730dc3d87cdb968
AndreRoj <130945779+AndreRoj@users.noreply.github.com>
2025-11-08 19:51:07 -0400

    paquete Clases

--- Sistemas Operativos/src/Clases/Usuario.java
+++ Sistemas Operativos/src/Clases/Usuario.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Estructuras;
+package Clases;
 import Tipos_de_Datos.TipoUsuario;
 /**
  *
